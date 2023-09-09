9e39c4a2e113146ad26953d6f33806be2bf4d1ee
nogi <Createc369.Re@gmail.com>
2023-09-09 13:01:48 +0900

    add: react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { SamplePage } from "./Pages/sample";
+import { AppPage } from "./pages/app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SamplePage />
+    {/* <SamplePage /> */}
+    <AppPage />
   </React.StrictMode>,
 );
