8c0b2e3501de5f8033ea233ce394b1a922aa345a
nogi <Createc369.Re@gmail.com>
2023-09-09 12:40:58 +0900

    add: prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { SamplePage } from './Pages/sample'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { SamplePage } from "./Pages/sample";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SamplePage />
   </React.StrictMode>,
-)
+);
